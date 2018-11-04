d3940945f461d30851e32f367210bc41cfbee3f8
Jeremy Roth <jeremy.roth.heig@gmail.com>
2018-11-04 11:28:08 +0100

    login implementation

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const config = require('../config');
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const utils = require('./utils');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY || 'aReallySecretKey';
 
 
 /* GET users */
@@ -42,8 +44,11 @@ userRouter.post('/', function(req, res, next) {
 });
 
 /* PATCH users -> remplace un user au complet*/
-userRouter.patch('/:id', utils.requireJson, loadUserFromParamsMiddleware, function(req, res, next) {
+userRouter.patch('/:id', authenticate, utils.requireJson, loadUserFromParamsMiddleware, function(req, res, next) {
   // Update properties present in the request body
+  if (req.user._id !== req.params.id){
+    return res.status(403).send('Please mind your own things.')
+  }
   if (req.body.username !== undefined) {
     req.user.username = req.body.username;
   }
@@ -121,4 +126,26 @@ function loadUserFromParamsMiddleware(req, res, next) {
 }
 function userNotFound(res, userId) {
   return res.status(404).type('text').send(`No user found with ID ${userId}`);
+}
+function authenticate(req, res, next) {
+  // Ensure the header is present.
+  const authorization = req.get('Authorization');
+  if (!authorization) {
+      return res.status(401).send('Authorization header is missing');
+  }
+  // Check that the header has the correct format.
+  const match = authorization.match(/^Bearer (.+)$/);
+  if (!match) {
+      return res.status(401).send('Authorization header is not a bearer token');
+  }
+  // Extract and verify the JWT.
+  const token = match[1];
+  jwt.verify(token, secretKey, function(err, payload) {
+      if (err) {
+          return res.status(401).send('Your token is invalid or has expired');
+      } else {
+          req.user._id = payload.sub;
+          next(); // Pass the ID of the authenticated user to the next middleware.
+      }
+  });
 }
