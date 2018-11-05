7fd3510e85134a3ad03779aac19e16acd8fb9b74
Dylan Fernandes <dylan.fernandes@heig-vd.ch>
2018-11-05 20:46:44 +0100

    modifPic

--- models/picture.js
+++ models/picture.js
@@ -3,35 +3,12 @@ const Schema = mongoose.Schema;
 
 // Define the schema for pictures
 const pictureSchema = new Schema({
-  inAlbum: {
-    type: ObjectId,
-    required: true,
-  },
+ 
   url: {
     type: String,
     required: true,
   },
-  addedBy: {
-    type: ObjectId,
-    required: true,
-  },
-  createdAt: {
-    type: String,
-    required: true,
-  },
-  location: {
-  	type: {
-  		type: {
-  			type: String,
-  			required: true,
-  			enum: ["Point"]
-  		},
-  		coordinates: {
-  			type: [Number],
-
-  		}
-  	}
-  }
+  
 });
 
 // Create the model from the schema and export it
